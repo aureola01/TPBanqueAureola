ee1de88e663c362c093bec8ed880aa40a6fdd8d1
aureola01 <aureolastudies@gmail.com>
2023-04-05 15:59:55 +0300

    TP4 - methode creer compte

--- src/main/java/mg/itu/tpbanqueaureola/ejb/GestionnaireCompte.java
+++ src/main/java/mg/itu/tpbanqueaureola/ejb/GestionnaireCompte.java
@@ -6,25 +6,34 @@ package mg.itu.tpbanqueaureola.ejb;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import mg.itu.tpbanqueaureola.entities.CompteBancaire;
 
 /**
  *
  * @author ahthan
  */
-@DataSourceDefinition (
-        className="com.mysql.cj.jdbc.MysqlDataSource", 
-        name="java:app/jdbc/banque", 
-        serverName="localhost", 
-        portNumber=3306, 
-        user="root", // nom et 
-        password="Password01!", // mot de passe que vous avez donnés lors de la création de la base de données 
-        databaseName="banque", 
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et 
+        password = "Password01!", // mot de passe que vous avez donnés lors de la création de la base de données 
+        databaseName = "banque",
         properties = {
-            "useSSL=false", 
-            "allowPublicKeyRetrieval=true" 
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true"
         }
 )
 @Stateless
 public class GestionnaireCompte {
-    
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
 }
