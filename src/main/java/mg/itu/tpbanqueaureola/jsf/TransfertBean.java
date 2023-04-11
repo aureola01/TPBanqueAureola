3edc047ba516f932f4332cfce4dd43156cac2353
aureola01 <aureolastudies@gmail.com>
2023-04-11 22:11:14 +0300

    TP4 - gestion des messages d'erreurs et succès

--- src/main/java/mg/itu/tpbanqueaureola/jsf/TransfertBean.java
+++ src/main/java/mg/itu/tpbanqueaureola/jsf/TransfertBean.java
@@ -8,6 +8,7 @@ import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import mg.itu.tpbanqueaureola.ejb.GestionnaireCompte;
+import mg.itu.tpbanqueaureola.entities.CompteBancaire;
 
 /**
  *
@@ -16,11 +17,11 @@ import mg.itu.tpbanqueaureola.ejb.GestionnaireCompte;
 @Named(value = "transfert")
 @RequestScoped
 public class TransfertBean {
-    
+
     private Long idSource;
     private Long idDestination;
     private int montant;
-    
+
     @EJB
     private GestionnaireCompte gestionCompte;
 
@@ -29,33 +30,57 @@ public class TransfertBean {
      */
     public TransfertBean() {
     }
-    
-    public Long getIdSource(){
+
+    public Long getIdSource() {
         return idSource;
     }
 
     public void setIdSource(Long idSource) {
         this.idSource = idSource;
     }
-    
-    public Long getIdDestination(){
+
+    public Long getIdDestination() {
         return idDestination;
     }
 
     public void setIdDestination(Long idDestination) {
         this.idDestination = idDestination;
     }
-    
-    public int getMontant(){
+
+    public int getMontant() {
         return montant;
     }
 
     public void setMontant(int montant) {
         this.montant = montant;
     }
-    
-    public String transferer(){
-        gestionCompte.transferer(gestionCompte.getCompte(idSource), gestionCompte.getCompte(idDestination), montant);
-        return "listeComptes?idS="+idSource+"&amp;idD="+idDestination+"&amp;montant="+montant+"&amp;faces-redirect=true";
+
+    public String transferer() {
+        boolean erreur = false;
+        CompteBancaire source = gestionCompte.getCompte(idSource);
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // pour le cas où le solde du compte source est insuffisant
+                Util.messageErreur("Le solde de l'envoyeur est insuffisant !", "Le solde de l'envoyeur est insuffisant !", "form:source");
+                erreur = true;
+            }
+        }
+        CompteBancaire destination = gestionCompte.getCompte(idDestination);
+        if (destination == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            erreur = true;
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+        gestionCompte.transferer(source, destination, montant);
+        // Message de succès ; addFlash à cause de la redirection.
+        Util.addFlashInfoMessage("Transfert correctement effectué");
+        return "listeComptes?idS=" + idSource + "&amp;idD=" + idDestination + "&amp;montant=" + montant + "&amp;faces-redirect=true";
     }
 }
